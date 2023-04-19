fa2b5d66880dcf4a6a0383df8846a74eb5ae39ea
Tyler Huyser <68932234+tylerhuyser@users.noreply.github.com>
2023-04-19 12:31:06 -0400

    Update divisibleSumPairs.js

--- divisibleSumPairs.js
+++ divisibleSumPairs.js
@@ -1,9 +1,9 @@
 function divisibleSumPairs(n, k, ar) {
     let count = 0
-    for (let i = 0; i < (i.length - 1); i++) {
-        for (let j = (i + 1); j < (i.length); j++) {
-            if (((ar[i] + ar[j]) % k) === 0) {
-                count ++
+    for (let i = 0; i < (ar.length - 1); i++) {
+        for (let j = (i + 1); j < (ar.length); j++) {
+            if ((ar[i] + ar[j]) % k === 0) {
+                count++
             }
         }
     }
