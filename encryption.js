ac6c4449e6016ce5b923c3e72d357d51d75e18b5
Tyler Huyser <68932234+tylerhuyser@users.noreply.github.com>
2024-10-19 13:37:51 -0400

    Create encryption.js

---
+++ encryption.js
@@ -0,0 +1,71 @@
+// Personal Solution
+
+function encryption(s) {
+    
+    let string = s
+    console.log(`String: ${string}`)
+    
+    let stringWithoutSpaces = s.split(" ").join("")
+    console.log(`String (Without Spaces): ${stringWithoutSpaces}`)
+    
+    let stringLength = stringWithoutSpaces.length
+    console.log(`String Length: ${stringLength}`)
+    
+    let squareRoot = Math.sqrt(stringLength)
+    console.log(`Square Root: ${squareRoot}`)
+    
+    let rows = Math.floor(squareRoot)
+    console.log(`Rows: ${rows}`)
+    
+    let columns = Math.ceil(squareRoot)
+    console.log(`Columns: ${columns}`)
+    
+    if (rows * columns < stringLength) {
+        rows = Math.ceil(squareRoot)
+    }
+    
+    let grid = []
+    let gridIterator = 0
+    
+    while (gridIterator < rows) {
+        grid[gridIterator] = stringWithoutSpaces.substring((gridIterator * columns), (gridIterator * columns + columns))
+        gridIterator++
+    }
+    
+    console.log(`Grid: ${grid}`)
+    
+    let encryptedGrid = []
+    console.log(`Encrypted Grid: ${encryptedGrid}`)
+    
+    let encryptionIterator = 0
+    console.log(`Initial Encrypted Iterator: ${encryptionIterator}`)
+    
+    while (encryptionIterator < columns) {
+        
+        let columnString = ""
+        console.log(`Current Encryption Iteration: ${encryptionIterator}`)
+        console.log(`Initial Column String: ${columnString}`)
+        
+        for (let row of grid) {
+            
+            console.log(`Current Character: ${row[encryptionIterator]}`)
+            if (row[encryptionIterator]) {
+                columnString += row[encryptionIterator]  
+            }
+        }
+
+        
+        encryptedGrid.push(columnString)
+        
+        console.log(`New Encrypted Row Pushed: ${encryptedGrid[encryptionIterator]}`)
+        
+        encryptionIterator++
+    }
+    
+    console.log(`Final Encrypted Grid : ${encryptedGrid}`)
+    
+    let encryptedString = encryptedGrid.join(" ")
+    console.log(`Final Encrypted String: ${encryptedString}`)
+
+    return encryptedString
+}
