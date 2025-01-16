05a46f9e284b226e9a42de3caaf3a653a2009dd7
Tyler Huyser <68932234+tylerhuyser@users.noreply.github.com>
2025-01-16 18:06:36 -0500

    Update grid-challenge.py

--- grid-challenge.py
+++ grid-challenge.py
@@ -1,14 +1,14 @@
 def gridChallenge(grid):
-  rows = len(grid)
-  coumns = len(grid[0])
+    rows = len(grid)
+    columns = len(grid[0])
 
-  for i in range(rows):
-    grid[i] = ''.join(sorted(grid[i]))
+    for i in range(rows):
+        grid[i] = ''.join(sorted(grid[i]))
 
-  for column in range(columns):
-    for row in range(rows - 1):
-      if (grid[row][column] > grid[row + 1][column]:
-          return "NO"
+    for column in range(columns):
+        for row in range(rows - 1):
+            if grid[row][column] > grid[row + 1][column]:
+                return "NO"
 
-  return "YES
+    return "YES"
 
