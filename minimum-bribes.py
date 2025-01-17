8c9ef35124ec0aad557eb234d066c0457533350a
Tyler Huyser <68932234+tylerhuyser@users.noreply.github.com>
2025-01-16 19:22:15 -0500

    Update minimum-bribes.py

--- minimum-bribes.py
+++ minimum-bribes.py
@@ -2,14 +2,14 @@ def minimum_bribes (q):
     queue = q
     bribes = 0
     
-    for person, index in enumerate(queue):
-        if index - (person + 1) > 2:
+    for index, person in enumerate(queue):
+        # print(person)
+        if person - (index + 1) > 2:
             print("Too chaotic")
             return
             
-        for j in range(max(0, index - 2, person)):
-            if queue[j] > index:
+        for j in range(max(0, person - 2, index)):
+            if queue[j] > person:
                 bribes += 1
                 
     print(bribes)
-
